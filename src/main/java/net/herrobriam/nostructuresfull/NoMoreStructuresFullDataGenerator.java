88b9cfc22fb081e17477cb064d05fc791f520fda
herrobriam <131239470+herrobriam@users.noreply.github.com>
2024-07-10 20:18:43 +0100

    Initial commit

---
+++ src/main/java/net/herrobriam/nostructuresfull/NoMoreStructuresFullDataGenerator.java
@@ -0,0 +1,11 @@
+package net.herrobriam.nostructuresfull;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NoMoreStructuresFullDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
